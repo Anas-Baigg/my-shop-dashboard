c95a24a3a102bb57e4c2e458055485e4fd82bb60
Anas baig <anasmirza748@gmail.com>
2026-01-15 16:20:40 +0000

    shadcn ui for  login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
+import { LoginForm } from "@/components/login-form";
 import React from "react";
 
 const login = () => {
   return (
-    <>
-      <h1 className="text-sky-400 ">login</h1>
-    </>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
+    </div>
   );
 };
 
