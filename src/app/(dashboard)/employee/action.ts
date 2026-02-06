9cb528dcbc4f504d6f4dd5034173d4f11b622916
Anas baig <anasmirza748@gmail.com>
2026-02-06 15:18:41 +0000

    create employee function

--- src/app/(dashboard)/employee/action.ts
+++ src/app/(dashboard)/employee/action.ts
@@ -8,3 +8,26 @@ export type ActionState = {
     success?: boolean;
   message?: string;
 } | null;
+
+export async function createEmployeeAction (shopId : string, formData: FormData):Promise<ActionState>{
+    const supabase = await createClient();
+    const name = formData.get("name") as string;
+    const passcode = formData.get("passcode") as string;
+
+    const nameV = validateEmployeeName(name);
+    const passV = validatePasscode(passcode);
+
+    if(!nameV.ok) return {message: nameV.message};
+    if(!passV.ok) return { message: passV.message };
+
+    const{error} = await supabase.from("employee").insert({
+        name: nameV.value.toUpperCase(),
+        passcode: passV.value,          
+        shop_id: shopId,
+    });
+
+    if (error) return {message : error.message};
+    revalidatePath("/employee");
+    return { success: true, message: "Employee added!" };
+
+}
